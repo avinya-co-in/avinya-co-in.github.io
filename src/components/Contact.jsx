ed2cb606560b2a48fd6153151235c050d60c1743
hardik88t <hardik88t@gmail.com>
2024-04-15 23:41:49 +0530

    Initiating project in Vite

---
+++ src/components/Contact.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="contact section-bg">
+      <div className="container">
+
+        <div className="section-title">
+          <h2>Contact</h2>
+          <p>Expand your business horizons with Avinya`s unparalleled expertise! Contact us now to embark on your journey to success in the Indian market.</p>
+        </div>
+
+        <div className="row">
+
+          <div className="col-lg-6">
+
+            <div className="row">
+              <div className="col-md-12">
+                <div className="info-box">
+                  <i className="bx bx-map"></i>
+                  <h3>Our Address</h3>
+                  <p>
+                    Avinya, Westgate,4, SG Hwy Service Rd, Ahmedabad, Gujarat, 380015
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="info-box mt-4">
+                  <i className="bx bx-envelope"></i>
+                  <h3>Email Us</h3>
+                  <p>
+                    <a href="mailto:avinya.marketing@gmail.com">avinya.marketing@gmail.com</a>
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="info-box mt-4">
+                  <i className="bx bx-phone-call"></i>
+                  <h3>Call Us</h3>
+                  <p>+91 96240 73799</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="col-lg-6 mt-4 mt-lg-0">
+            <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                </div>
+                <div className="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                </div>
+              </div>
+              <div className="form-group mt-3">
+                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+              </div>
+              <div className="form-group mt-3">
+                <textarea className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div className="my-3">
+                <div className="loading">Loading</div>
+                <div className="error-message"></div>
+                <div className="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div className="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
