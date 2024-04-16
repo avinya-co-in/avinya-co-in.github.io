cfad0de62fd2fa7a548deb6daa7304c435455998
hardik88t <hardik88t@gmail.com>
2024-04-16 23:26:35 +0530

    Commiting project in Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home.jsx";
+
+function App() {
+  console.log("HELLO");
+
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
