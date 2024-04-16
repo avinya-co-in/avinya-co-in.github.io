a04fd09fdf1032b38611706b65affb20cc1395fb
hardik88t <hardik88t@gmail.com>
2024-04-16 23:26:35 +0530

    Commiting project in Vite

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+// Home.jsx
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Features from "../components/Features";
+import Services from "../components/Services";
+import WhyUs from "../components/WhyUs";
+import FAQs from "../components/FAQs";
+import Contact from "../components/Contact";
+import WhatsAppIcon from "../components/WhatsAppIcon";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <main id="main">
+        <About />
+        <WhyUs />
+        <Services />
+        <Features />
+        <FAQs />
+        <Contact />
+      </main>
+      <WhatsAppIcon />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
